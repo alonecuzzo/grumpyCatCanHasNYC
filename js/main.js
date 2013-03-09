039e73b8a48782beadf9dac359873e36cd2c58b7
Jabari Bell <jabari.bell@pxlflu.net>
2013-03-08 21:51:26 -0500

    moved bizness to mainjs

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//our game business
+function init() {
+	// get a reference to the canvas we'll be working with:
+	var canvas = document.getElementById("testCanvas");
+
+	// create a stage object to work with the canvas. This is the top level node in the display list:
+	var stage = new createjs.Stage(canvas);
+
+	// Create a new Text object:
+	var text = new createjs.Text("GRUMPY CAT CAN HAS SAVE TEH CITY!", "36px Arial", "#777");
+
+	// add the text as a child of the stage. This means it will be drawn any time the stage is updated
+	// and that it's transformations will be relative to the stage coordinates:
+	stage.addChild(text);
+
+	// position the text on screen, relative to the stage coordinates:
+	text.x = 200;
+	text.y = 200;
+
+	// call update on the stage to make it render the current display list to the canvas:
+	stage.update();
+}
