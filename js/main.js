624e0447e159f1f4ec8b15c2d11e70b7ca8ce658
Zainab 3brahimi <zainabebrahimi@gmail.com>
2013-03-09 13:14:56 -0500

    up/down key alerts

--- js/main.js
+++ js/main.js
@@ -48,13 +48,18 @@ function handleComplete() {
 
 		switch(id){
 			case "grumpyCat":
+				// grumpyCat = new createjs.Shape();
+				// var gC= grumpyCat.graphics;
+				// gC.beginBitmapFill(result);
+				// gC.drawRect(0, 0, 100, 79);
 				grumpyCat = new createjs.Bitmap(result);
-				grumpyCat.x= 200;
+				grumpyCat.x = w - (w-100);
+				grumpyCat.y = 20;
 				stage.addChild(grumpyCat);
 				break;
 			case "unicorn":
 				unicorn = new createjs.Bitmap(result);
-				unicorn.x = 700;
+				unicorn.x = w - 500;
 				stage.addChild(unicorn);
 				break;
 			case "cheezburger":
@@ -62,13 +67,14 @@ function handleComplete() {
 				cheezburger.x = 400;
 				cheezburger.y = 50;
 				stage.addChild(cheezburger);
+				break;
 			case "buildings":
 				buildings = new createjs.Bitmap(result);
 				buildings.x = 500;
 				stage.addChild(buildings);
+				break;
 		}
 		
-			
 	
 	}
 
@@ -76,14 +82,48 @@ function handleComplete() {
 	// grumpyCat = loader.getResult("grumpyCat");
 	// stage.addChild(grumpyCat);
 
-	stage.update();
+	// stage.update();
 
-	// createjs.Ticker.setFPS(30);
-	// createjs.Ticker.addEventListener("tick", tick);
+	createjs.Ticker.setFPS(40);
+	createjs.Ticker.addEventListener("tick", tick);
 }
 
-// function tick(event) {
+function checkArrows (field, evt) {
+  	var keyCode = 
+    document.layers ? evt.which :
+    document.all ? event.keyCode :
+    document.getElementById ? evt.keyCode : 0;
 
-// 	stage.update(event);
+	var r = '';
+
+	  if (keyCode == 39)
+	    r += 'arrow right';
+	  else if (keyCode == 40)
+	    r += 'arrow down';
+	  else if (keyCode == 38)
+	    r += 'arrow up';
+	  else if (keyCode == 37)
+	    r += 'arrow left';
+	  r += ' ' + keyCode;
+	  
+	  return r;
+}
+
+// function grumpyMove(){
+// 	grumpyCat.
 // }
+
+
+function tick(event) {
+
+
+	var arrow = checkArrows(stage, event);
+	
+	if(arrow != undefined){
+		alert("hey");
+	}
+
+	grumpyCat.y = (grumpyCat.y + 2);
+	stage.update(event);
+}
 
